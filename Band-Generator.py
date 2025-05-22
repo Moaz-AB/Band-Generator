ce97d9adc39009322271fd8d7f7578a178414780
Moaz Abuzaid <moazabuzaid9@gmail.com>
2025-05-21 23:39:51 -0400

    Create Band-Generator.py
    A simple custom band generator project.

---
+++ Band-Generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator!")
+city=input("What is the name of the city you grew up in?")
+pet=input("What was the name of your first pet?")
+print("Your band name could be "+city+" "+pet)
+print("Cool!")
